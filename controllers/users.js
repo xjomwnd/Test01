8a1c9566367423bd47ae5dac5f34c096ed48cdb2
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-03 10:33:20 +0300

    commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,71 +1,28 @@
-const express = require('express');
-const app = express();
-const usersRouter = require('./controllers/users');
+const usersData = require('../data/users')
+const User = require('../model/user')
 
-// Use the users router
-app.use('/users', usersRouter);
-
-// Set up other configurations and middleware
-
-// Get port from environment and store in Express
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-// Create HTTP server
-const server = http.createServer(app);
-
-// Listen on provided port, on all network interfaces
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-// Normalize a port into a number, string, or false
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
+exports.getUsers = (req, res, next) => {
+    User.find().then(users => {
+        res.status(200).json({
+            message: "Users fetched successfully",
+            users: users
+        })
+    })
 }
 
-// Event listener for HTTP server "error" event
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-// Event listener for HTTP server "listening" event
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+exports.postUser = async (req, res, next) => {
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        age: req.body.age,
+    })
+    user.save().then(user => {
+        res.status(201).json({
+            message: "User created successfully",
+            userId: user._id
+        })
+    }).catch(err => {
+        console.log(err)
+    })
 }
